242c39d4a9a03540cb82b8709cdc651eb22c10df
ivanznaorhr-design <ivan.znaor.hr@gmail.com>
2025-11-12 20:43:20 +0100

    Hajduk karta: lokalni map.html + toolbar + sites.geojson

--- app/src/main/java/com/example/hajduci/MainActivity.java
+++ app/src/main/java/com/example/hajduci/MainActivity.java
@@ -10,20 +10,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         WebView webView = new WebView(this);
+
         WebSettings s = webView.getSettings();
         s.setJavaScriptEnabled(true);
         s.setDomStorageEnabled(true);
         s.setAllowFileAccess(true);
         s.setAllowContentAccess(true);
-        s.setAllowUniversalAccessFromFileURLs(true);
-        s.setAllowFileAccessFromFileURLs(true);
+        // omogućimo file->file resurse u WebViewu
+        try {
+            s.setAllowFileAccessFromFileURLs(true);
+            s.setAllowUniversalAccessFromFileURLs(true);
+        } catch (Throwable ignored) {}
+
         webView.setWebViewClient(new WebViewClient());
-
-        // >>> OVO JE KLJUČNO <<<
-        webView.loadUrl("file:///android_asset/map.html");
-
         setContentView(webView);
+
+        webView.loadUrl("file:///android_asset/map.html");
     }
 }
